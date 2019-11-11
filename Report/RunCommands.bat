e5fbc9f8b7c0c41ebc29427728edfdb12fc40a39
freeac <nhanvuong.tbqn@hotmail.com>
2019-11-11 15:51:34 +0700

    add jar file and change content bat file

--- Report/RunCommands.bat
+++ Report/RunCommands.bat
@@ -12,6 +12,12 @@ set Sec=%dt:~12,2%
 
 java -jar Reporthtml.jar "%cd%/VOOC_Report_Automation_%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 
+java -jar CompressFile.jar 1 "C:\temp\report\Report_APi_%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%.zip" "%cd%/VOOC_Report_Automation_%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
+
 java -jar MoveFile.jar "%cd%/VOOC_Report_Automation_%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%" "C:\temp\report\VOOC_Report_Automation_%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%"
 
 java -jar MoveFile.jar "%cd%/..\Logs\Log.txt" "C:\temp\log\Log_%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%.txt"
+
+java -jar SendMail.jar "C:\temp\report\Report_APi_%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%.zip" nhanvuong@vooc.vn
+
+java -jar DeleteFile.jar 1 "C:\temp\report\Report_APi_%YYYY%-%MM%-%DD%_%HH%-%Min%-%Sec%.zip"
